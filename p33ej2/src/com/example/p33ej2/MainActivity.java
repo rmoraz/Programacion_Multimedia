52e5d7b2f290fe8c56988d85765ee7572f5e9e6c
rmoraz <rmorazgarcia@gmail.com>
2014-11-24 14:40:43 +0100

    p33ej2

--- p33ej2/src/com/example/p33ej2/MainActivity.java
+++ p33ej2/src/com/example/p33ej2/MainActivity.java
@@ -4,25 +4,33 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import android.app.Activity;
+import java.util.ArrayList;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
+	ArrayList<String> lista = new ArrayList<String>();
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] canciones = null;
+
+        ListView listView = getListView();
+ 		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+ 		listView.setTextFilterEnabled(true);
+        String linea;
         int i = 0;
         
         try {
-        	// open the file for reading
         	InputStream instream = new FileInputStream("/data/canciones.txt");
+        	// open the file for reading
 
         	// if file the available for reading
         	if (instream != null) {
@@ -31,21 +39,42 @@ public class MainActivity extends Activity {
         	  BufferedReader buffreader = new BufferedReader(inputreader);
 
         	  // read every line of the file into the line-variable, on line at the time
-        	  do {
-        	     canciones[i] = buffreader.readLine();
-        	     
-        	     i++;
+        	  while((linea = buffreader.readLine()) != null){
+        	     lista.add(linea); 
         	    // do something with the line 
-        	  } while (canciones[i] != null);
-
+        	  };
         	}
         	} catch (Exception ex) {
         	    // print stack trace.
-        	} finally {
-        	// close the file.
-        	//instream.close();        	
-        	}
+        	} 
+        
+ 		
+        setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_checked, android.R.id.text1, lista));
+  
+ 		// Assign adapter to ListView
+		//listView.setAdapter(adapter);
         
     }
+    
+    public void onListItemClick(
+    	    ListView parent, View v, int position, long id)
+    	    {
+    	        Toast.makeText(this,
+    	            "Has seleccionado " + lista.get(position),
+    	            Toast.LENGTH_SHORT).show();
+    	    }
+    	    
+    	    public void onClick(View view) {
+    	    	ListView listView = getListView();
+    	    	
+    	    	String itemsSelected = "Canciones seleccionadas: \n";
+    	    	for (int i=0; i<listView.getCount(); i++) {
+    	    		if (listView.isItemChecked(i)) {
+    	    			itemsSelected += listView.getItemAtPosition(i) + "\n";
+    	    		}
+    	    	}
+    	    	Toast.makeText(this, itemsSelected, Toast.LENGTH_LONG).show();
+    	    }
 
 }
